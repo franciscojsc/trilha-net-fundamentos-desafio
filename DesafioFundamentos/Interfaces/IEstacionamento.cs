3904c4b0aa558fb2f9416c4b90f74759f888f5cc
Francisco Chaves <francisco.jschaves@gmail.com>
2024-05-05 23:11:20 -0300

    refactor:  add documentation comments

--- DesafioFundamentos/Interfaces/IEstacionamento.cs
+++ DesafioFundamentos/Interfaces/IEstacionamento.cs
@@ -7,12 +7,27 @@ namespace DesafioFundamentos.Interfaces
 {
     public interface IEstacionamento
     {
+        /// <summary>
+        /// Adiciona veículo no estacionamento
+        /// </summary>
         public void AdicionarVeiculo();
 
+        /// <summary>
+        /// Remove veículo no estacionamento
+        /// </summary>
         public void RemoverVeiculo();
 
+        /// <summary>
+        /// Lista os veículos no estacionamento
+        /// </summary>
         public void ListarVeiculos();
 
+        /// <summary>
+        /// Processa o valor do pagamento pelo estacionamento
+        /// </summary>
+        /// <param name="precoInicial"></param>
+        /// <param name="precoPorHora"></param>
+        /// <returns></returns>
         public decimal ProcessarPagamento(decimal precoInicial, decimal precoPorHora);
     }
 }
